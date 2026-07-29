5a99c091f29ef9754578bedeb511cf8472c90b2f
aasc150978-ux <aasc150978@gmail.com>
2026-07-28 22:12:50 -0300

    Add files via upload

---
+++ src/aula.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {View,text,Stylesheet} from 'react-native';
+
+class Aula01 extends Component {
+    render () {
+        return(
+
+        )
+    }
+}
